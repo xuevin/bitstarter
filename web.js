33111a69482ed93a1dab4e127350c3ba63185924
xue.vin <xue.vin@gmail.com>
2013-07-12 19:50:44 +0000

    test change to view from index

--- web.js
+++ web.js
@@ -3,8 +3,14 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  indexBuffer = fs.readFile('index.html')
+  indexString = indexBuffer.toString()
+  response.send(indexString)
+  //response.send('Hello World2!');
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
